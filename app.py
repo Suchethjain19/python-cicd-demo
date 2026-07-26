a89f50560d0420bab666b736a4cc4e89d44e41af
Suchethjain19 <jainsucheth123@gmail.com>
2026-07-26 14:06:29 +0530

    Update welcome message version in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>🚀 Welcome to My AWS CI/CD Project - Version 2</h1>"
+    return "<h1>🚀 Welcome to My AWS CI/CD Project - Version 1</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
