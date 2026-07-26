203682af78d7578b7ac62c7ef8dc48c7e3eca9fe
suchethjain19 <jainsucheth123@gmail.com>
2026-07-26 19:20:09 +0530

    Test pipeline

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "<h1>🚀 Welcome to My AWS CI/CD Project - Versionn 66</h1>"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+print("pipeline test")
