8c27d82cfdd17afd9fb1a48b4b8cfc5fef27d718
Suchethjain19 <jainsucheth123@gmail.com>
2026-08-14 15:06:21 +0530

    Update greeting message in home route

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-
-
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins CI/CD ,SUCHETH YOU CAN DO IT"
+    return "Hello from Jenkins CI/CD ...,SUCHETH YOU CAN DO IT"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
